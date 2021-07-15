e7eaae659c05daf4c0408124e06fa078d9cf56d5
rvillada <rvillada18@gmail.com>
2021-07-15 13:51:07 -0500

    Users can signup

--- app/src/main/java/com/codepath/tripplannerapp/LoginActivity.java
+++ app/src/main/java/com/codepath/tripplannerapp/LoginActivity.java
@@ -19,6 +19,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignupInitiate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +33,9 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignupInitiate = findViewById(R.id.btnSignupInitiate);
+
+        // Listener for login button
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,6 +43,16 @@ public class LoginActivity extends AppCompatActivity {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 loginUser(username, password);
+            }
+        });
+
+        // Listener for signup button
+        btnSignupInitiate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(i);
+                finish();
             }
         });
     }
@@ -53,7 +67,7 @@ public class LoginActivity extends AppCompatActivity {
                     return;
                 }
                 goMainActivity();
-                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(LoginActivity.this, "Successfully logged in!", Toast.LENGTH_SHORT).show();
                 Log.i(TAG, "success with login");
             }
         });
---
+++ app/src/main/java/com/codepath/tripplannerapp/SignupActivity.java
@@ -0,0 +1,68 @@
+package com.codepath.tripplannerapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignupActivity extends AppCompatActivity {
+
+    public static final String TAG = "SignupActivity";
+    private EditText etUsernameSignup;
+    private EditText etPasswordSignup;
+    private Button btnSignupOfficial;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        etUsernameSignup = findViewById(R.id.etUsernameSignup);
+        etPasswordSignup = findViewById(R.id.etPasswordSignup);
+        btnSignupOfficial = findViewById(R.id.btnSignupOfficial);
+
+        btnSignupOfficial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick signup button");
+
+                String username = etUsernameSignup.getText().toString();
+                String password = etPasswordSignup.getText().toString();
+
+                // Create the ParseUser
+                ParseUser user = new ParseUser();
+
+                user.setUsername(username);
+                user.setPassword(password);
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e != null) {
+                            Toast.makeText(SignupActivity.this, "Issue with login!", Toast.LENGTH_SHORT).show();
+                            return;
+                        } else {
+                            goMainActivity();
+                            Toast.makeText(SignupActivity.this, "Successfully signed up!", Toast.LENGTH_SHORT).show();
+                            Log.i(TAG, "success with signup");
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
