eab7f44e9bda7469f0c8c64d72f32cdedd78b61b
rvillada <rvillada18@gmail.com>
2021-07-15 16:20:19 -0500

    User can browse photos from gallery from the create new trip activity page. Selected photo from the gallery will appear on the create new trip page.

--- app/src/main/java/com/codepath/tripplannerapp/NewTripActivity.java
+++ app/src/main/java/com/codepath/tripplannerapp/NewTripActivity.java
@@ -1,11 +1,16 @@
 package com.codepath.tripplannerapp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.parse.Parse;
@@ -16,7 +21,10 @@ import com.parse.SaveCallback;
 public class NewTripActivity extends AppCompatActivity {
 
     public static final String TAG = "NewTripActivity";
+    private static final int RESULT_LOAD_IMAGE = 1;
     private EditText etTripName;
+    private ImageView ivTripImage;
+    private Button btnUploadTripImage;
     private Button btnNewTripSubmit;
 
     @Override
@@ -25,6 +33,8 @@ public class NewTripActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_trip);
 
         etTripName = findViewById(R.id.etTripName);
+        ivTripImage = findViewById(R.id.ivTripImage);
+        btnUploadTripImage = findViewById(R.id.btnUploadTripImage);
         btnNewTripSubmit = findViewById(R.id.btnNewTripSubmit);
 
         btnNewTripSubmit.setOnClickListener(new View.OnClickListener() {
@@ -40,8 +50,26 @@ public class NewTripActivity extends AppCompatActivity {
             }
         });
 
+        btnUploadTripImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
 
 
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            ivTripImage.setImageURI(selectedImage);
+
+        }
     }
 
     private void saveTrip(String tripName, ParseUser currentUser) {
